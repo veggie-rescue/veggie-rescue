3f350f5c696f3e981554f21e4c6085f17994a5d8
5-Fifths <btruong648@gmail.com>
2026-03-11 17:51:22 -0700

    Fetch works and data displays partially
     - feat: data is able to be fetched from the sheet using the http://localhost:3000/testing endpoint
     - feat: location and name columns display properly
     - bug: everything between location and name doesn't display at all
        - 1. consider endpoints: location and name are the last and first elements
        - 2. consider key names: name and location are single words and thus don't have camelcase to throw off the keys

--- apps/web/src/app/recipients/page.tsx
+++ apps/web/src/app/recipients/page.tsx
@@ -1,8 +1,35 @@
+"use client";
+import { useState, useEffect } from 'react';
+import { DataTable } from '@/components/DataTable/DataTable';
+
 export default function Recipients() {
+    const [sheetData, setSheetData] = useState([]);
+
+    useEffect(() => {
+        // TODO: adjust fetch route when the backend route changes
+        async function fetchData() {
+            const res = await fetch('http://localhost:3000/testing');
+            const data = await res.json();
+
+            setSheetData(data);
+        }
+
+        fetchData();
+    }, []);
+
+    // Ensure that the sheet data was fetched propely
+    if (!sheetData || sheetData.length <= 0) {
+        return (
+            <DataTable headers={['No Data Found.']} data={[]} />
+        )
+    }
+
+    const headers = Object.keys(sheetData[0]);
+
     return (
         <>
             <h1>Recipients</h1>
-            <p>Coming Soon</p>
+            <DataTable headers={headers} data={sheetData} />
         </>
     );
 }
